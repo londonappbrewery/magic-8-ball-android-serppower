a7366fb7c648b7011b94f81ee2958f1d6823c75d
Spark <ahmedsalah60@gmail.com>
2017-11-15 08:20:23 +0200

    Hi Please check my work :)

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,13 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button magic_button;
+        magic_button = (Button) findViewById(R.id.magic_button);
+
+        final ImageView ball = (ImageView) findViewById(R.id.image_ball);
+
+        TextView title = (TextView) findViewById(R.id.text_title);
+
+        final int[] ballArray = {R.drawable.ball1,
+        R.drawable.ball2,
+        R.drawable.ball3,
+        R.drawable.ball4,
+        R.drawable.ball5
+        };
+
+        magic_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Magic", "It's Working!");
+
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(5);
+                Log.d("Magic!","Your Answer is: " + number);
+
+                ball.setImageResource(ballArray[number]);
+
+
+            }
+        });
+
     }
 }
